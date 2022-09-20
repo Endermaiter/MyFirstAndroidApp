8f5a6e57f78f3930006feb60875f1ad249a61867
plagocastro <plagocastro@danielcastelao.org>
2022-09-20 09:28:21 +0200

    Cada vez que entra en pausa,indica el tiempo que estuvo activa

--- app/src/main/java/com/marcos/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/marcos/myfirstandroidapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.marcos.myfirstandroidapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    var horaInicial: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) { //la interrogacion significa que la
+                                                        // variable llamada savedInstanceState, de tipo Bundle, puede ser "null""
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Estado","Oncreate")
+        horaInicial = System.currentTimeMillis()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val horaFinal:Long = System.currentTimeMillis()
+        val t = (horaFinal-horaInicial)/1000
+        Log.d("Time","Time lapsed = $t s")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Estado","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("Estado","onResume")
     }
 }
