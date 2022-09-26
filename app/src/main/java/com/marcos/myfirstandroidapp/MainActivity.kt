0941cc203361da32804e9902cb713cca56ee2604
plagocastro <plagocastro@danielcastelao.org>
2022-09-26 09:13:03 +0200

    Indica en un texto por pantalla el timpo que estuvo activa la app siempre que le des al boton

--- app/src/main/java/com/marcos/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/marcos/myfirstandroidapp/MainActivity.kt
@@ -1,36 +1,58 @@
 package com.marcos.myfirstandroidapp
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //LA APLICACION NOS DICE EL TIEMPO QUE LA APLICACION ESTUVO PAUSADA CUANDO LE DAMOS AL BOTON DE
+    // TIME, REINICIANDOSE EL CONTADOR DE TIEMPO CADA VEZ QUE VOLVEMOS A ELLA DE NUEVO
 
     var horaInicial: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) { //la interrogacion significa que la
-                                                        // variable llamada savedInstanceState, de tipo Bundle, puede ser "null""
+        // variable llamada savedInstanceState, de tipo Bundle, puede ser "null""
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Estado","Oncreate")
-        horaInicial = System.currentTimeMillis()
+        Log.d("Estado", "Oncreate")
+        val miBoton:Button = findViewById(R.id.containedButton)
+        val miTexto:TextView = findViewById(R.id.textView)
+        miBoton.text = "SHOW TIME PAUSED"
+        miTexto.text = "¡BIENVENIDO!"
     }
 
     override fun onPause() {
         super.onPause()
-        val horaFinal:Long = System.currentTimeMillis()
-        val t = (horaFinal-horaInicial)/1000
-        Log.d("Time","Time lapsed = $t s")
+        horaInicial = System.currentTimeMillis()
+
+        val miTexto:TextView = findViewById(R.id.textView)
+        miTexto.text = "MANTEN LA APP PAUSADA DURANTE UNOS SEGUNDOS"
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("Estado","onStart")
+        Log.d("Estado", "onStart")
+        val horaFinal: Long = System.currentTimeMillis()
+        val t = (horaFinal - horaInicial) / 1000
+        Log.d("Time", "Time lapsed = $t s")
+
+        val miBoton:Button = findViewById(R.id.containedButton)
+        val miTexto:TextView = findViewById(R.id.textView)
+
+        miBoton.setOnClickListener {
+            miTexto.text = "La aplicacion estuvo pausada $t seg"
+        }
     }
 
     override fun onResume() {
         super.onResume()
-
-        Log.d("Estado","onResume")
+        Log.d("Estado", "onResume")
+        val miTexto:TextView = findViewById(R.id.textView)
     }
+
+
 }
